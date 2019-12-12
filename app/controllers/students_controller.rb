c5fd326bb26485936d08e1ba0a43d065ac43f561
Ariel <arielord@buffalo.edu>
2019-12-12 16:37:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,19 +1,33 @@
 class StudentsController < ApplicationController
   def index
+    @students = Sctudent.all
   end
   
   def show
+    @student = Student.find(params[:id])
   end
   
   def new
+    @student = Student.new()
   end
   
   def create
+    @student = Student.create(post_params(:title, :room_number))
+    redirect_to school_class_path(@student)
   end
   
   def edit
+    @student = Student.find(params[:id])
   end
   
   def update
+    @student = Student.find(params[:id])
+    @student.update(post_params(:title, :room_number))
+  end
+  
+  private
+  
+  def post_params(*args)
+    params.require(:student).permit(*args)
   end
 end
